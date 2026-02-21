07b97cc7d1b2d8d0ae5192b61b14b8e896ab2395
mihirzalavadiya <mihirzalavadiya@gmail.com>
2026-02-21 17:14:00 +0530

    add jwt token

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const User = require('./models/user');
+const bycrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const {
   validateSignUpData,
   validateLoginData,
   validateEditProfileData,
 } = require('./utils/validation');
-const bycrypt = require('bcrypt');
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post('/signup', async (req, res) => {
   try {
@@ -41,6 +44,8 @@ app.post('/login', async (req, res) => {
     const isPasswordValid = await user.validatePassword(password);
 
     if (isPasswordValid) {
+      const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+      res.cookie('token', token);
       res.status(200).send({ message: 'Login successful' });
     } else {
       res.status(400).send({ message: 'Invalid credentials' });
@@ -50,12 +55,45 @@ app.post('/login', async (req, res) => {
   }
 });
 
+app.get('/profile', async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).send({ message: 'Unauthorized' });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded._id;
+    const user = await User.findById(userId).select('-password');
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+    const userProfile = {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      skills: user.skills,
+      photoURL: user.photoURL,
+    };
+
+    res.status(200).send(userProfile);
+  } catch (err) {
+    res
+      .status(500)
+      .send({ message: 'Error fetching profile', error: err.message });
+  }
+});
+
 app.get('/feed', async (req, res) => {
   try {
     const users = await User.find();
-    res.status(200).send(users);
+    const userFeed = users.map((user) => ({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      skills: user.skills,
+      photoURL: user.photoURL,
+    }));
+    res.status(200).send(userFeed);
   } catch (err) {
-    res.status(500).send({ message: 'Error fetching users', error: err });
+    res.status(500).send({ error: err });
   }
 });
 
